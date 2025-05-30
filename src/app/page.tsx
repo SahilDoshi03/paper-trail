8fd231517439a9ead22a2bc031cd28f7363d6d94
sahil-dsh <sahil.doshi@logixal.com>
2025-05-30 13:14:14 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import EditorComponent from "./components/EditorComponent";
+
+export default function Home() {
+  return (
+    <div className="">
+      Hello
+      <EditorComponent/>
+    </div>
+  );
+}
