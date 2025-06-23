1f234feedaf833d46ab579455c9a023969716728
Sahil Doshi <sahil.doshi@logixal.com>
2025-06-23 13:00:39 +0530

    make font family utils and put loading of google fonts into that

--- src/app/components/SecondaryHeader/CustomFontSelector.tsx
+++ src/app/components/SecondaryHeader/CustomFontSelector.tsx
@@ -1,14 +1,24 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import type { WebFont } from '@/app/types/Font'
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import type { WebFont } from "@/app/types/Font";
+import { CustomEditor } from "@/app/utils/CustomEditor";
+import { useSlate } from "slate-react";
+import { loadGoogleFont } from "@/app/utils/FontFamily";
 
-const CustomFontSelector = () => {
-  const [fontsList, setFontsList] = useState([])
+const CustomFontSelector = ({
+  setFonts,
+  setFontFamily,
+}: {
+  setFonts: Dispatch<SetStateAction<string[]>>;
+  setFontFamily: Dispatch<SetStateAction<string>>;
+}) => {
+  const editor = useSlate();
+  const [fontsList, setFontsList] = useState([]);
 
   useEffect(() => {
     const loadFonts = async () => {
-      const res = await fetch('/api/fonts');
+      const res = await fetch("/api/fonts");
       const data = await res.json();
       setFontsList(data.items || []);
     };
@@ -18,7 +28,12 @@ const CustomFontSelector = () => {
   return (
     <div className="modal">
       <div className="flex justify-between items-center">
-        <input id="font-search" type="search" placeholder="Search Fonts" className="border-1 p-2 rounded-sm"/>
+        <input
+          id="font-search"
+          type="search"
+          placeholder="Search Fonts"
+          className="border-1 p-2 rounded-sm"
+        />
         <div className="flex gap-4">
           <div>Scripts</div>
           <div>Show</div>
@@ -27,13 +42,26 @@ const CustomFontSelector = () => {
       </div>
 
       <div className="mt-4 h-100 border-1 overflow-auto space-y-2 rounded-sm">
-        {fontsList.length > 0 ?
+        {fontsList.length > 0 ? (
           fontsList.map((font: WebFont) => (
-            <li key={font.family} className="p-2 border-b border-gray-200">
+            <li
+              key={font.family}
+              className="p-2 border-b border-gray-200"
+              onClick={() => {
+                loadGoogleFont(font.family);
+                setFontFamily(font.family);
+                setFonts((prev: string[]) => [...prev, font.family]);
+                CustomEditor.setFontFamily(editor, font.family);
+              }}
+            >
               {font.family}
             </li>
-          )) : <>Loading...</>}
+          ))
+        ) : (
+          <>Loading...</>
+        )}
       </div>
-    </div>)
-}
-export default CustomFontSelector
+    </div>
+  );
+};
+export default CustomFontSelector;
--- src/app/components/SecondaryHeader/FontsPopover.tsx
+++ src/app/components/SecondaryHeader/FontsPopover.tsx
@@ -5,6 +5,7 @@ import Modal from "../common/Modal";
 import CustomFontSelector from "./CustomFontSelector";
 import { CustomEditor } from "@/app/utils/CustomEditor";
 import { useSlate } from "slate-react";
+import { loadGoogleFont } from "@/app/utils/FontFamily";
 
 const FontsPopover = ({
   setFontFamily,
@@ -12,14 +13,14 @@ const FontsPopover = ({
   setFontFamily: Dispatch<SetStateAction<string>>;
 }) => {
   const editor = useSlate();
-  const fonts = ["Arial", "Calibri", "Times New Roman"];
+  const [fonts, setFonts] = useState(["Arial", "Times New Roman"]);
   const [customModalOpen, setCustomModalOpen] = useState(false);
 
   return (
     <>
       <ul className="flex flex-col w-max">
         <li
-          className="border-b p-2 cursor-pointer"
+          className="border-b p-2 hover:bg-[#222222] cursor-pointer"
           onClick={() => setCustomModalOpen(true)}
         >
           Custom
@@ -31,6 +32,7 @@ const FontsPopover = ({
             style={{ fontFamily: font }}
             onClick={() => {
               setFontFamily(font);
+              loadGoogleFont(font);
               CustomEditor.setFontFamily(editor, font);
             }}
           >
@@ -43,7 +45,12 @@ const FontsPopover = ({
           title={"Fonts"}
           open={customModalOpen}
           onClose={() => setCustomModalOpen(false)}
-          children={<CustomFontSelector />}
+          children={
+            <CustomFontSelector
+              setFonts={setFonts}
+              setFontFamily={setFontFamily}
+            />
+          }
         />
       )}
     </>
