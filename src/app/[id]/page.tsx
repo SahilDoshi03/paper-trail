722c087eabc3c469389718865c4650bc645e53b9
sahildsh03 <sahildsh03@gmail.com>
2025-07-17 22:13:09 +0530

    Add google sign in using next auth

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,11 +2,14 @@
 
 import EditorComponent from "@/app/components/EditorComponent/EditorComponent";
 import { getDocument, updateDocument } from "@/app/actions/Document";
-import { DocumentType } from "@/app/types/Document";
+import { DocumentType } from "@/app/lib/schemas/Document";
 import { notFound, useParams } from "next/navigation";
 import { useState, useRef, useEffect } from 'react';
+import { signIn, useSession } from "next-auth/react";
 
 export default function Document() {
+  const { data: sessionData } = useSession()
+  console.log(sessionData)
   const params = useParams()
   const [docValue, setDocValue] = useState<DocumentType | null>(null);
   const [loading, setLoading] = useState(true);
@@ -64,21 +67,30 @@ export default function Document() {
 
   return (
     <div className="p-10">
-      {isEditing ? (
-        <input
-          ref={inputRef}
-          type="text"
-          value={currentTitle}
-          onChange={handleTitleChange}
-          onBlur={handleTitleBlur}
-          onKeyDown={handleKeyDown}
-          className="text-[2rem] mb-4 outline-none border-b border-gray-300 focus:border-blue-500"
-        />
-      ) : (
-        <h2 className="text-[2rem] mb-4 cursor-pointer" onClick={handleTitleClick}>
-          {currentTitle}
-        </h2>
-      )}
+      <div className="flex justify-between">
+        {isEditing ? (
+          <input
+            ref={inputRef}
+            type="text"
+            value={currentTitle}
+            onChange={handleTitleChange}
+            onBlur={handleTitleBlur}
+            onKeyDown={handleKeyDown}
+            className="text-[2rem] mb-4 outline-none border-b border-gray-300 focus:border-blue-500"
+          />
+        ) : (
+          <h2 className="text-[2rem] mb-4 cursor-pointer" onClick={handleTitleClick}>
+            {currentTitle}
+          </h2>
+        )}
+        <form
+          action={async () => {
+            await signIn("google")
+          }}
+        >
+          <button type="submit">Signin with Google</button>
+        </form>
+      </div>
       {docValue && (
         <div className="flex flex-col gap-10 items-center justify-center">
           <EditorComponent docId={docId} docValue={docValue} />
